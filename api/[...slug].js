008edd7b302cc90a6fbcbae13c94f3360878a6bf
Aman Gulia <amangulia4610@gmail.com>
2025-07-27 23:05:44 -0400

    Remove vercel-env-variables.txt file containing environment variable configurations.

--- api/[...slug].js
+++
@@ -1,5 +0,0 @@
-// Main API handler for all routes
-import app from '../server/index.js';
-
-// Export the Express app as a serverless function
-export default app;
--- api/category/[...slug].js
+++
@@ -1,8 +0,0 @@
-import '../../server/config/connectDB.js';
-import app from '../../server/index.js';
-
-export default async function handler(req, res) {
-  // Set the correct path for the Express app
-  req.url = `/api/category${req.url}`;
-  return app(req, res);
-}
